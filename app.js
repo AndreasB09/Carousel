5f9df184006cd22f7962eec704a5e7b385e73664
andreasb09 <andreasb09.kodehode@gmail.com>
2024-04-29 13:03:19 +0200

    working carousel

---
+++ app.js
@@ -0,0 +1,23 @@
+document.getElementById("next").addEventListener("click", function () {
+  changeSlide(1);
+});
+
+document.getElementById("previous").addEventListener("click", function () {
+  changeSlide(-1);
+});
+
+function changeSlide(direction) {
+  const images = document.querySelectorAll('#carousel img');
+  let currentIndex = -1;
+  images.forEach((img, index) => {
+    if (img.classList.contains('active')) {
+      currentIndex = index;
+    }
+  });
+
+  images[currentIndex].classList.remove('active');
+
+  let newIndex = (currentIndex + direction + images.length) % images.length;
+
+  images[newIndex].classList.add('active');
+}
