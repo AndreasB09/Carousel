baa4478f94574c05bd1d078805aaf70f9b03512f
andreasb09 <andreasb09.kodehode@gmail.com>
2024-05-06 14:58:34 +0200

    *

--- app.js
+++ app.js
@@ -29,3 +29,25 @@ let itemsContainer = document.querySelector(".items-container");
 let pressed = false;
 let startX;
 let x;
+
+mousedragContainer.addEventListener("mousedown", (e) => {
+  pressed = true;
+  startX = e.offsetX - itemsContainer.offsetLeft;
+  mousedragContainer.style.cursor = "grabbing";
+})
+
+mousedragContainer.addEventListener("mouseenter", () => {
+  mousedragContainer.style.cursor = "grab";
+})
+
+mousedragContainer.addEventListener("mouseup", () => {
+  mousedragContainer.style.cursor = "grab";
+  pressed = false;
+})
+
+mousedragContainer.addEventListener("mousemove", (e) => {
+  if (!pressed) return;
+  e.preventDefault();
+  x = e.offsetX;
+  itemsContainer.style.left = `${x - startX}px`;
+})
